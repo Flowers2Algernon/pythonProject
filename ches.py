63ec89c7c7664777d99cb030676d3fd33aa74d44
zhujinhong <zjhong6666@gmail.com>
2024-08-07 21:15:41 +0800

    change file relative path

--- ches.py
+++ ches.py
@@ -1,3 +1,3 @@
 import numpy as np
-data = np.loadtxt(fname='/Users/zhujinhong/PycharmProjects/pythonProject/data mining/data/inflammation-01.csv', delimiter=',')
+data = np.loadtxt(fname='data mining/data/inflammation-01.csv', delimiter=',')
 print(data)
