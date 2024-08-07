48f8c7cd518a247ebcfd3430593434b8a695e9e4
zhujinhong <zjhong6666@gmail.com>
2024-08-07 22:17:13 +0800

    add dtype

--- ches.py
+++ ches.py
@@ -1,3 +1,11 @@
 import numpy as np
 data = np.loadtxt(fname='data mining/data/inflammation-01.csv', delimiter=',')
 print(data)
+print(type(data))
+#find out the type of the data contained in the Numpy array.
+print(data.dtype)
+#print data's shape
+print(data.shape)
+print('first value in data:', data[0,0])
+print('middle value in data:', data[30,20])
+print('last value in data:', data[-1,-1])
